e384930cf8fe957ccf09a3a919348e9827bdbcc9
pocketlinux32 <pocketlinux32@gmail.com>
2021-03-04 09:47:14 -0600

    Update to package manager

--- pls-pkg.py
+++ pls-pkg.py
@@ -4,18 +4,26 @@ import requests os
 randomSeed = seed()
 
 def getPackage(packageName):
-	deps = requests.get("https://pocketlinux32.000webhostap.com/package-deps/" + packageName)
-	if deps.status_code == 404:
+	request = requests.get("http://pocketlinux32.000webhostapp.com/package-deps/" + packageName)
+	if request.status_code == 404:
 		print("Warning: Failed to download dependencies (not found), assuming it has none.")
-	elif deps.status_code >= 500:
+	elif request.status_code >= 500:
 		print("Error: Failed to download dependencies due to a remote server error")
 		exit(3)
-	elif deps.status_code >= 400:
+	elif request.status_code >= 400:
 		print("Error: Failed to download dependencies due to a internal system error")
 		exit(3)
 
-	for i in deps:
+	for i in request:
 		getPackage(i)
+
+	request = requests.get("http://pocketlinux32.000webhostapp.com/packages/" + packageName)
+	if request.status_code == 404:
+		print("Error: Package not found in remote server"
+		exit(4)
+	elif request.status_code >= 500:
+		print("Error: Failed to downloads package")
+	file = open(packageName, "w")
 
 print("Package manager, Version 0.02")
 print("(c)2021 pocketlinux32, Under GPLv3")
